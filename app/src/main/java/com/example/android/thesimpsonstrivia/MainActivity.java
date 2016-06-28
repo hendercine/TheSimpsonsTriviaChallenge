adfb609a283fae4c470f2ab1821b556479d33e0b
hendercine <james@hendercine.com>
2016-06-27 20:02:51 -0700

    Add question 2 answer to MainActivity Java

--- app/src/main/java/com/example/android/thesimpsonstrivia/MainActivity.java
+++ app/src/main/java/com/example/android/thesimpsonstrivia/MainActivity.java
@@ -26,19 +26,56 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
 
         /**
-         * This checks the users question 1 answer
+         * This checks the users question 1 answer and adds a points if correct
          */
 
         RadioButton question_1_answer = (RadioButton) findViewById(R.id.question_1_answer);
         boolean correctOne = question_1_answer.isChecked();
 
-        EditText enterNameField = (EditText) findViewById(R.id.order_name_view);
-        Editable orderName = enterNameField.getText();
+        if (correctOne) {
+            quizScore = quizScore + 1;
+        }
 
+        /**
+         * This checks the users question 2 answer and adds a points if correct
+         */
 
+        CheckBox question_2_answer_1 = (CheckBox) findViewById(R.id.question_2_answer_1);
+        boolean correctTwoOne = question_2_answer_1.isChecked();
 
-        CheckBox chocolateToppingCheckBox = (CheckBox) findViewById(R.id.chocoalte_topping_checkbox);
-        boolean hasChocolate = chocolateToppingCheckBox.isChecked();
+        CheckBox question_2_answer_2 = (CheckBox) findViewById(R.id.question_2_answer_2);
+        boolean correctTwoTwo = question_2_answer_2.isChecked();
+
+        if (correctTwoOne) while (correctTwoTwo) {
+            quizScore = quizScore + 1;
+        }
+
+        /**
+         * This checks the users question 3 answer
+         */
+
+        RadioButton quest3on_1_answer = (RadioButton) findViewById(R.id.quest3on_1_answer);
+        boolean corThreetOne = quest3on_1_answer.isChecke
+
+        if (correctThree) {
+            quizScore = quizScore + 1;d();
+
+        /**
+         * This checks the users question 4 answer
+         */
+
+        RadioButton question_1_answer = (RadioButton) findViewById(R.id.question_1_answer);
+        boolean correctOne = question_1_answer.isChecke
+
+        d();
+
+        /**
+         * This checks the users question 5 answer
+         */
+
+        RadioButton question_1_answer = (RadioButton) findViewById(R.id.question_1_answer);
+        boolean correctOne = question_1_answer.isChecked.getText();
+
 
         int price = calculatePrice(hasWhip, hasChocolate);
         String priceMessage = createOrderSummary(orderName, price, hasWhip, hasChocolate);
