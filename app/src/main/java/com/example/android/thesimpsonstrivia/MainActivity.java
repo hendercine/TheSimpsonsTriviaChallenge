f69d363bcae509234a1c9ab0d65e6d204568a5ce
hendercine <james@hendercine.com>
2016-06-27 19:19:00 -0700

    Finish questions on xml layout

--- app/src/main/java/com/example/android/thesimpsonstrivia/MainActivity.java
+++ app/src/main/java/com/example/android/thesimpsonstrivia/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quizScore = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
